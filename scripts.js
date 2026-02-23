b29bdba79df38b59f742a4cc8dcefc2475e3d3d4
Maged Groups <magedyaseengroups@gmail.com>
2026-02-23 16:14:18 +0200

    feat: services created

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 console.log('I am happy :)');
 const vat = 0.14;
+
+
+const services_list = [
+    'Mobile Apps',
+    'Web Apps',
+    'Marketing',
+    'Social Media Management',
+]
+
+services_list.forEach(service => services.innerHTML += `<h3>${service}</h3>`)
